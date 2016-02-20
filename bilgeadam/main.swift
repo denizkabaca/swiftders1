49a258f0b653c9b22b75e87f5abace4b57b09101
IOS 3401 <ios3401@lab-g-imac-8.local>
2016-02-20 09:55:28 +0200

    Initial Commit

---
+++ bilgeadam/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  bilgeadam
+//
+//  Created by IOS 3401 on 20/02/16.
+//  Copyright © 2016 IOS 3401. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
